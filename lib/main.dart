3736e3835970f54a9a1e50780e42d581455a2569
Puja Kumari Singh <80578996+PujaSingh014@users.noreply.github.com>
2023-01-17 03:26:54 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -25,35 +25,3 @@ Future<void> main() async {
 
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  bool _isSignedIn = false;
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getUserLoggedInStatus();
-  }
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: _isSignedIn ? HomePage() : WelcomePage(),
-    );
-  }
-
-  void getUserLoggedInStatus() async{
-    await HelperFunctions.getUserLoggedInStatus().then((value) {
-      if(value!=null){
-        _isSignedIn = value;
-      }
-    });
-  }
-}
